8d495ff844d71fc3f76a1e1fcc52a09aaf06cd3e
Priyodarshighos <priyodarshi.ghosh@bitcoin.com>
2024-11-29 19:15:26 +0530

    creating logic to generate/import using private key and mnemonic

--- src/main/java/com/example/SolanaPOC/controller/WalletController.java
+++ src/main/java/com/example/SolanaPOC/controller/WalletController.java
@@ -1,11 +1,11 @@
 package com.example.SolanaPOC.controller;
 
 import com.example.SolanaPOC.core.Account;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,15 +14,55 @@ public class WalletController {
 
     @GetMapping("/create")
     public Map<String, String> createWallet() {
-        // Generate a new Solana account (wallet)
         Account account = new Account();
+        Map<String, String> wallet = new HashMap<>();
+        wallet.put("publicKey", account.getPublicKeyBase58());
+        wallet.put("privateKey", account.getPrivateKeyBase58());
+        return wallet;
+    }
 
-        // Create a response map with public and private keys
+    @PostMapping("/import/bip44")
+    public Map<String, String> importFromBip44(@RequestBody Map<String, String> body) {
+        List<String> words = Arrays.asList(body.get("mnemonic").split(" "));
+        String passphrase = body.getOrDefault("passphrase", "");
+
+        Account account = Account.fromBip44Mnemonic(words, passphrase);
+
         Map<String, String> wallet = new HashMap<>();
         wallet.put("publicKey", account.getPublicKeyBase58());
         wallet.put("privateKey", account.getPrivateKeyBase58());
 
-        return wallet; // Return the wallet details as JSON
+        return wallet;
     }
+
+    @PostMapping("/import/bip44-change")
+    public Map<String, String> importFromBip44Change(@RequestBody Map<String, String> body) {
+        List<String> words = Arrays.asList(body.get("mnemonic").split(" "));
+        String passphrase = body.getOrDefault("passphrase", "");
+
+        Account account = Account.fromBip44MnemonicWithChange(words, passphrase);
+
+        Map<String, String> wallet = new HashMap<>();
+        wallet.put("publicKey", account.getPublicKeyBase58());
+        wallet.put("privateKey", account.getPrivateKeyBase58());
+
+        return wallet;
+    }
+
+    @PostMapping("/import/privateKey")
+    public Map<String, String> importFromPrivateKey(@RequestBody Map<String, String> body) {
+        String privateKey = body.get("privateKey");
+
+        Account account = Account.fromBase58PrivateKey(privateKey);
+
+        Map<String, String> wallet = new HashMap<>();
+        wallet.put("publicKey", account.getPublicKeyBase58());
+        wallet.put("privateKey", account.getPrivateKeyBase58());
+
+        return wallet;
+    }
+
+
+
 }
 
